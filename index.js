19b9468f467113eacf5d14b329ac1194f097b68c
Darlan Alves <me@darlanalv.es>
2018-12-07 17:45:23 +0100

    feat(*): add names to bots

--- index.js
+++ index.js
@@ -4,37 +4,121 @@ const Path = require('path');
 const WebSocket = require('ws');
 const config = require('./config.json');
 
+class Storage {
+  constructor(path) {
+    this.path = path;
+    this.read();
+  }
+
+  getItem(name) {
+    return this.storage[name];
+  }
+
+  setItem(name, value) {
+    this.storage[name] = value;
+    this.write();
+  }
+
+  deleteItem(name) {
+    delete this.storage[name];
+    this.write();
+  }
+
+  hasItem(name) {
+    return name in this.storage;
+  }
+
+  write() {
+    try {
+      const json = JSON.stringify(this.storage);
+      FS.writeFileSync(this.path, json);
+    } catch (e) {
+      console.warn(`Failed to save ${this.path}!`);
+    }
+  }
+
+  read() {
+    try {
+      const text = FS.readFileSync(this.path).toString();
+      this.storage = text ? JSON.parse(text) : {};
+    } catch (e) {
+      this.storage = {};
+      this.write();
+    }
+  }
+}
+
 const nodes = {
   WEB: 'web',
   BOT: 'bot',
   web: [],
-  bot: []
+  bot: [],
+
+  nameStore: new Storage('./bot-names.json')
 };
 
 const httpServer = http.createServer(function(request, response) {
-  if (request.url === '/') {
+  const url = request.url;
+  const method = request.method;
+
+  if (url === '/') {
     FS.createReadStream('./web/index.html').pipe(response);
     return;
   }
 
+  if (serveFile(request, response)) {
+    return;
+  }
+
+  if (url === '/status') {
+    getStatus(request, response);
+    return;
+  }
+
+  if (url === '/bots') {
+    getBotList(request, response);
+    return;
+  }
+
+  if (url.startsWith('/bot/') && method === 'POST') {
+    return renameBot(request, response);
+  }
+
+  response.writeHead(404);
+  response.end('Not found');
+});
+
+function serveFile(request, response) {
   const pwd = process.cwd();
   const filePath = Path.normalize(request.url.replace(/\.\./g, ''));
   const absPath = Path.join(pwd, 'web', filePath);
 
   if (FS.existsSync(absPath)) {
     FS.createReadStream(absPath).pipe(response);
-    return;
+    return true;
+  }
+}
+
+function renameBot(request, response) {
+  const botId = request.url.slice(5);
+  const bot = nodes.bot.filter(x => x.id === botId)[0];
+  let buffer = '';
+
+  if (!bot) {
+    response.writeHead(404);
+    response.end('Not found');
   }
 
-  if (request.url === '/status') {
-    response.writeHead(200);
-    response.end(getStatus());
-    return;
-  }
+  request.on('data', (s) => buffer += s);
+  request.on('end', () => {
+    const name = buffer.slice(0, 32);
+    bot.name = name;
+    nodes.nameStore.setItem(botId, name);
 
-  response.writeHead(404);
-  response.end('Not found');
-});
+    response.writeHead(204);
+    response.end('Renamed');
+  });
+}
 
 const botSocket = new WebSocket.Server({
   httpServer,
@@ -48,8 +132,19 @@ const webSocket = new WebSocket.Server({
   path: config.web.path
 });
 
-function relay(origin) {
+function relay(origin, connection) {
   return function (payload) {
+    if (origin === nodes.BOT && String(payload).startsWith('bot::')) {
+      const id = payload.slice(5).slice(0, 32);
+      connection.id = id;
+
+      if (nodes.nameStore.hasItem(id)) {
+        connection.name = nodes.nameStore.getItem(id);
+      }
+
+      return;
+    }
+
     const target = origin === nodes.WEB ? nodes.bot : nodes.web;
     target.forEach(node => node.send(payload));
   }
@@ -70,14 +165,34 @@ function handleConnection(type) {
     const target = type === nodes.WEB ? nodes.web : nodes.bot;
     target.push(connection);
     connection.on('close', disconnect(type, connection));
-    connection.on('message', relay(type));
+    connection.on('message', relay(type, connection));
   };
 }
 
-function getStatus() {
-  return `
-  Bots (${nodes.bot.length})
-  Web Clients (${nodes.web.length})`;
+function toJSON(x) {
+  return JSON.stringify(x, null, 2);
+}
+
+function getBotList(_, response) {
+  const list = nodes.bot.map(x => {
+    return {
+      id: x.id,
+      name: (x.name || '')
+    };
+  });
+
+  response.writeHead(200);
+  response.end(toJSON(list));
+}
+
+function getStatus(_, response) {
+  const status = {
+    bots: nodes.bot.length,
+    clients: nodes.web.length
+  };
+
+  response.writeHead(200);
+  response.end(toJSON(status));
 }
 
 botSocket.on('connection', handleConnection(nodes.BOT));
